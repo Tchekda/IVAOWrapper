2a23919dedb07fd8306ff6fec3ba7b1768543d3e
Tchekda (ASUS) <contact@tchekda.fr>
2020-04-05 00:10:56 +0200

    New sub-version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="IVAOWrapper",
-    version="0.1.2",
+    version="0.1.3",
     author="Tchekda",
     author_email="contact@tchekda.fr",
     description="Light Python 3 Wrapper for IVAO Network",
